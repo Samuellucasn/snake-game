3474c11f612760b1878d0e651eee9b01a5d8fea2
Samuellucasn <samuellucasnogueira@gmail.com>
2022-11-03 17:00:01 -0300

    update

--- main.js
+++ main.js
@@ -1,101 +1,104 @@
 const area = document.querySelector(".area")
 const context = area.getContext("2d")
 
-let bloco = 5
+let block = 7
 
 let positionX = 10
 let positionY = 10
-let macaX = 20
-let macaY = 10
-let velocidadeX = 0
-let velocidadeY = 0
+let appleX = 20
+let appleY = 10
+let speedX = 0
+let speedY = 0
 
-let cobra = []
-cobra.push({
+let snake = []
+
+snake.push({
     x: positionX,
     y: positionY
 })
-let calda = 5
 
-setInterval(jogo, 90)
+let tail = 5
+
+setInterval(game, 90)
 
 document.addEventListener("keydown", (e) => {
     switch(e.keyCode) {
         case 37:
-            if (velocidadeX != 1) {
-            velocidadeX = -1
-            velocidadeY = 0
+            if (speedX != 1) {
+            speedX = -1
+            speedY = 0
             }
         break
         case 38:
-            if (velocidadeY != -1) {
-            velocidadeX = 0
-            velocidadeY = 1
+            if (speedY != -1) {
+            speedX = 0
+            speedY = 1
             }
         break
         case 39:
-            if (velocidadeX != -1) {
-            velocidadeX = 1
-            velocidadeY = 0
+            if (speedX != -1) {
+            speedX = 1
+            speedY = 0
             }
         break
         case 40:
-            if (velocidadeY != 1){
-            velocidadeX = 0
-            velocidadeY = -1
+            if (speedY != 1){
+            speedX = 0
+            speedY = -1
             }
         break
     }
 })
 
-function criaBackground() {
+function makeBackground() {
     context.fillStyle = "black"
-    context.fillRect(0, 0, area.clientWidth, area.clientHeight/2) 
+    context.fillRect(0, 0, area.clientWidth, area.clientHeight) 
 }
 
-function criaMaca() {
+function makeApple() {
     context.fillStyle = "red"
-    context.fillRect(macaX * bloco, macaY * bloco, bloco, bloco/2 )
+    context.fillRect(appleX * block, appleY * block, block, block )
 }
 
-function criaCobra() {
-    for (let i = 0; i < cobra.length; i++){
+function makeSnake() {
+    for (let i = 0; i < snake.length; i++){
         context.fillStyle = "green"
-        context.fillRect(cobra[i].x * bloco, cobra[i].y * bloco, 5 * 2, 5)
-        if (cobra[i].x === positionX && cobra[i].y === positionY) {
-            velocidadeX = velocidadeY = 0
-            calda = 5
+        context.fillRect(snake[i].x * block, snake[i].y * block, block, block)
+        if (snake[i].x === positionX && snake[i].y === positionY) {
+            speedX = speedY = 0
+            tail = 5
         }
     }
 }
 
 
-function jogo() {
+function game() {
     
-    positionX += velocidadeX
-    positionY -= velocidadeY
+    positionX += speedX
+    positionY -= speedY
 
-    if (positionX < 0) {positionX = 58}
-    if (positionX > 58) {positionX = 0}
-    if (positionY < 0) {positionY = 29}
-    if (positionY > 29) {positionY = 0}
+    if (positionX < 0) {positionX = 56}
+    if (positionX > 56) {positionX = 0}
+    if (positionY < 0) {positionY = 56}
+    if (positionY > 56) {positionY = 0}
 
-    criaBackground()
-    criaMaca()
-    criaCobra() 
+    makeBackground()
+    makeApple()
+    makeSnake() 
     
-    cobra.push({
+    snake.push({
         x: positionX,
         y: positionY
     })
-    while (cobra.length > calda) {
-        cobra.shift()
+
+    while (snake.length > tail) {
+        snake.shift()
     }
 
-    if (macaX===positionX && macaY === positionY) {
-        calda++
-        macaX = Math.floor(Math.random() * 58)
-        macaY = Math.floor(Math.random() * 29)
+    if (appleX === positionX && appleY === positionY) {
+        tail++
+        appleX = Math.floor(Math.random() * 58)
+        appleY = Math.floor(Math.random() * 29)
     }
 
 }
